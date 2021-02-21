523358122ed46b8942afbf2dabaa19a0736e6cba
kosmolet <svet1199@gmail.com>
2021-02-21 17:35:15 +0100

    add track and schemas

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
+import { TrackModule } from './track/track.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), TrackModule],
   controllers: [AppController],
   providers: [AppService],
 })
